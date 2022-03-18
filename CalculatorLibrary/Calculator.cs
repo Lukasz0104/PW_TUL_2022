7fdb69706b4eee8bdce8f57003174428843918f7
Łukasz Stępień <lukasz.01.stepien@gmail.com>
2022-03-18 17:25:00 +0100

    Sample program

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        public int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Concurrent programming
+
+## Working Group
+
+| Name Surname (initials) | GUID |
+| ----------------------- | ---- |
+| Łukasz Stępień | `{E021D0A9-9B7B-45C9-82D0-F92C21A66AD5}` |
+| FS | `{7D295F5F-346C-4B92-86CD-50DE1E2D3F96}` |
